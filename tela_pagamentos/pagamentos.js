fe8f288963fdd18f84128d30c755b73d17588076
jessicammelo <jessica.08041992@gmail.com>
2021-10-19 21:49:09 -0300

    tela pagamentos

--- tela_pagamentos/pagamentos.js
+++ tela_pagamentos/pagamentos.js
@@ -1,18 +1,45 @@
-$(document).on("click", ".side-menu > ul > li", function(e){
-  if($(this).find('.sub-menu').length>0){
-      e.preventDefault();
-      e.stopPropagation();
-      $(this).toggleClass('active');
-      $(this).find('.sub-menu').stop().slideToggle();
-  }
-  
-});
+function myFunction2() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-$(document).on('change', '.animateInput input', function() {
-  var input = $(this);
-  if (input.val().length) {
-      input.addClass('populated');
-  } else {
-      input.removeClass('populated');
-  }
-});
+function menuBold(className) {
+document.getElementsByClassName(className)[0].classList.add("menu-bold");
+}
+
+function dropdown(){
+  myFunction2();
+  menuBold(className);
+}
+
+function inicio() {
+  showInicio();
+  menuBold('item1');
+}
+
+function showInicio(){
+  document.getElementById('inicio').classList.add('visible');
+  document.getElementById('transferencia').classList.remove('visible');
+  document.getElementById('contaCorrente').classList.remove('visible');
+}
+
+function contaCorrente() {
+  showContaCorrente();
+  menuBold('item2');
+}
+
+function showContaCorrente(){
+  document.getElementById('transferencia').classList.remove('visible');
+  document.getElementById('inicio').classList.remove('visible');
+  document.getElementById('contaCorrente').classList.add('visible');
+}
+
+function transferencia() {
+  showTransferencia();
+  menuBold('item3');
+}
+
+function showTransferencia(){
+  document.getElementById('inicio').classList.remove('visible');
+  document.getElementById('contaCorrente').classList.remove('visible');
+  document.getElementById('transferencia').classList.add('visible');
+}
