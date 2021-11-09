39795417e56cd2a1ea9040aac768d7103a39225d
jessicammelo <jessica.08041992@gmail.com>
2021-11-09 20:52:11 -0300

    tela pagamentos

--- trabalho-final/src/app/page-pagamentos/page-pagamentos.module.ts
+++ trabalho-final/src/app/page-pagamentos/page-pagamentos.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { PagePagamentosComponent } from './page-pagamentos.component';
 import { PagamentosRoutingModule } from './page-pagamentos-routing.module';
+import { MenuSidebarComponent } from '../menu-sidebar/menu-sidebar.component';
 
 @NgModule({
   declarations: [
-    PagePagamentosComponent
+    PagePagamentosComponent,
+    MenuSidebarComponent
   ],
   imports: [
     CommonModule,
